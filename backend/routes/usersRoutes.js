721ac2f52d9827a07eed3a7bc4d175b420a8a90c
ziann <cheongziann@gmail.com>
2024-01-09 23:10:40 +0800

    add register function in the future

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -7,5 +7,6 @@ const {getUsers, getUser, loginUser} = require("../controllers/usersController")
 router.get("/", getUsers)
 router.get("/getone/:id", getUser)
 router.post("/login", loginUser)
+// router.post("/register", registerUser)
 
 module.exports = router
